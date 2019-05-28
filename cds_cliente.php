14c4982e74a388f383d50bdc9ebdf7cd411eabf4
nezaodooco <bruce_gabriel_rossini@hotmail.com>
2019-05-28 20:51:29 -0300

    Create cds_cliente.php

---
+++ cds_cliente.php
@@ -0,0 +1,19 @@
+<?php include "conexao_banco.php";
+
+$nome=$_POST['nome_cliente'];
+$sobrenome=$_POST['sobrenome_cliente'];
+$cpf=$_POST['cpf_cliente'];
+$telefone=$_POST['telefone_cliente'];
+$rua_cliente=$_POST['end_rua_cliente'];
+$num_cliente=$_POST['end_num_cliente'];
+$bairro_cliente=$_POST['end_bairro_cliente'];
+$cidade=$_POST['cidade_cliente'];
+$estado=$_POST['estado_cliente'];
+$num_pedido=$_POST['num_pedido'];
+
+$sql=mysqli_query($conexao, "INSERT INTO clientes (nome_cliente, sobrenome_cliente, cpf_cliente, telefone_cliente, end_rua_cliente, end_num_cliente, end_bairro_cliente, cidade_cliente, estado_cliente, num_pedido) VALUES ('$nome', '$sobrenome', '$cpf', '$telefone', '$rua_cliente', '$num_cliente', '$bairro_cliente', '$cidade', '$estado', '$num_pedido')");
+
+echo"<script>window.location='cadastro_clientes.php';
+alert('Gravado com Sucesso!');
+</script>";
+?>
