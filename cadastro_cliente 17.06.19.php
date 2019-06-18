c36bb529e6f8fd0aad3e70d16c46beb20623248c
nezaodooco <bruce_gabriel_rossini@hotmail.com>
2019-06-18 20:27:41 -0300

    cad_cliente
    18/06/2019

--- cadastro_cliente 17.06.19.php
+++ cadastro_cliente 17.06.19.php
@@ -327,35 +327,100 @@
           </ul>
 
         </nav>
-
 		<div class="container">
-			<div class="row justify-content-center align-items-center">
-        <form name="cadastro_cliente" method="post" action="cds_cliente.php">
-            <fieldset>
-                <legend>Cadastro</legend>
-                <div class="cadastro" >
-                    <label>     Nome:</label><input type="text" placeholder="Digite o Nome..." name="nome_cliente"><br>
-                    <label>Sobrenome:</label><input type="text" placeholder="Digite o Sobrenome..." name="sobrenome_cliente"><br>
-                    <label>      CPF:</label><input type="text" placeholder="Digite o Cpf..." name="cpf_cliente"><br>
-                    <label> Telefone:</label><input type="text" placeholder="Digite o Telefone..." name="telefone_cliente"><br>
-                    <label>   Cidade:</label><input type="text" placeholder="Digite a Cidade..." name="cidade_cliente"><br>
-                    <label>   Estado:</label><input type="text" placeholder="Digite o Estado..." name="estado_cliente"><br>
-                    <label> Endereço:</label><input type="text" placeholder="Digite o Endereço..." name="end_rua_cliente">
-                    <label>   Número:</label><input type="text" placeholder="Digite o Número..." name="end_num_cliente"><br>
-                    <label>   Bairro:</label><input type="text" placeholder="Digite o Bairro..." name="end_bairro_cliente"><br>
-                    <input type="submit" class="btn-group" value="Cadastrar" >
-                    <input type="reset" class="btn-group" value="Limpar" >
-                </div> 
-            </fieldset>
-        </form>
-	 </div>
-	</div>
+			<form name="cadastro_cliente" method="post" action="cds_cliente.php">
+
+			 	<div class="form-row">
+			   		<div class="form-group col-md-4">
+			      		<label for="inputName">Nome</label>
+			      		<input type="text" class="form-control" name="nome_cliente">
+			    	</div>
+			    	<div class="form-group col-md-4">
+			      		<label for="inputSobrenome">Sobrenome</label>
+			      		<input type="text" class="form-control" name="sobrenome_cliente">
+			    	</div>
+			  		<div class="form-group col-md-4">
+			    		<label for="inputAddress">CPF</label>
+			    		<input type="text" class="form-control" name="cpf_cliente">
+			  		</div>
+			  	</div>
+
+			  	<div class="form-row">
+			   		<div class="form-group col-md-4">
+			      		<label for="inputTel">Telefone</label>
+			      		<input type="text" class="form-control" name="telefone_cliente">
+			    	</div>
+			    </div>
+
+			  	<div class="form-row">
+			  		<div class="form-group col-md-6">
+			    		<label for="inputAddress2">Endereço</label>
+			    		<input type="text" class="form-control" name="end_rua_cliente" placeholder="Rua...">
+			  		</div>
+			  		<div class="form-group col-md-1">
+			    		<label for="inputAddress2">Número</label>
+			    		<input type="text" class="form-control" name="end_num_cliente" placeholder="Nº">
+			  		</div>
+			  	</div>
+
+			  	<div class="form-row">
+			  		<div class="form-group col-md-3">
+			     		<label for="inputCity">CEP</label>
+			      		<input type="text" class="form-control" name="cep_cliente">
+			      	</div>
+			      	<div class="form-group col-md-3">
+			     		<label for="inputEstado">Estado</label>
+			      		<select id="inputEstado" class="form-control">
+			        		<option> </option>
+						    <option value="ac">Acre</option> 
+						    <option value="al">Alagoas</option> 
+						    <option value="am">Amazonas</option> 
+						    <option value="ap">Amapá</option> 
+						    <option value="ba">Bahia</option> 
+						    <option value="ce">Ceará</option> 
+						    <option value="df">Distrito Federal</option> 
+						    <option value="es">Espírito Santo</option> 
+						    <option value="go">Goiás</option> 
+						    <option value="ma">Maranhão</option> 
+						    <option value="mt">Mato Grosso</option> 
+						    <option value="ms">Mato Grosso do Sul</option> 
+						    <option value="mg">Minas Gerais</option> 
+						    <option value="pa">Pará</option> 
+						    <option value="pb">Paraíba</option> 
+						    <option value="pr"selected>Paraná</option> 
+						    <option value="pe">Pernambuco</option> 
+						    <option value="pi">Piauí</option> 
+						    <option value="rj">Rio de Janeiro</option> 
+						    <option value="rn">Rio Grande do Norte</option> 
+						    <option value="ro">Rondônia</option> 
+						    <option value="rs">Rio Grande do Sul</option> 
+						    <option value="rr">Roraima</option> 
+						    <option value="sc">Santa Catarina</option> 
+						    <option value="se">Sergipe</option> 
+						    <option value="sp">São Paulo</option> 
+						    <option value="to">Tocantins</option> 
+						</select>
+			    	</div>
+			    	<div class="form-group col-md-3">
+			     		<label for="inputCity">Cidade</label>
+			      		<input type="text" class="form-control" id="inputCity">
+			      	</div>
+			  	</div>
+			  	<div class="form-group">
+			    	<div class="form-check">
+			      		<input class="form-check-input" type="checkbox" id="gridCheck">
+			      		<label class="form-check-label" for="gridCheck">Clique em mim</label>
+			    	</div>
+			  	</div>
+			  	<button type="submit" class="btn btn-primary">Entrar</button>
+			</form>
+		</div>
     
 	<!-- Footer -->
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2019</span>
+            <span>Copyright &copy; Pedro Corno</span>
           </div>
         </div>
       </footer>
