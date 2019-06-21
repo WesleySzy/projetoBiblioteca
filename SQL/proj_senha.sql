6446b164d1fa83321905fe6b666f531775b8e85a
nezaodooco <bruce_gabriel_rossini@hotmail.com>
2019-06-21 00:41:04 -0300

    Atualização - Banco
    21/06/2019

--- SQL/proj_senha.sql
+++ SQL/proj_senha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 15-Jun-2019 às 06:54
+-- Generation Time: 21-Jun-2019 às 05:40
 -- Versão do servidor: 10.1.40-MariaDB
 -- versão do PHP: 7.1.29
 
@@ -40,16 +40,37 @@ CREATE TABLE `clientes` (
   `end_num_cliente` int(5) NOT NULL,
   `end_bairro_cliente` varchar(20) NOT NULL,
   `cidade_cliente` varchar(25) NOT NULL,
-  `estado_cliente` varchar(2) NOT NULL,
-  `num_pedido` int(9) NOT NULL
+  `estado_cliente` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `clientes`
 --
 
-INSERT INTO `clientes` (`codigo_cliente`, `nome_cliente`, `sobrenome_cliente`, `cpf_cliente`, `telefone_cliente`, `end_rua_cliente`, `end_num_cliente`, `end_bairro_cliente`, `cidade_cliente`, `estado_cliente`, `num_pedido`) VALUES
-(1, 'fsdafasd', 'sdafsdaf', 544646556, '46456456', '', 4464, 'dsadsads', 'dsadsadasdsa', 'ds', 4545);
+INSERT INTO `clientes` (`codigo_cliente`, `nome_cliente`, `sobrenome_cliente`, `cpf_cliente`, `telefone_cliente`, `end_rua_cliente`, `end_num_cliente`, `end_bairro_cliente`, `cidade_cliente`, `estado_cliente`) VALUES
+(1, 'fsdafasd', 'sdafsdaf', 544646556, '46456456', 'fdsfds', 4464, 'dsadsads', 'dsadsadasdsa', 'ds'),
+(2, '', '', 0, '', '', 0, '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id_produto` int(11) NOT NULL,
+  `nome_produto` varchar(50) NOT NULL,
+  `valor_produto` decimal(10,2) NOT NULL,
+  `obs_produto` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id_produto`, `nome_produto`, `valor_produto`, `obs_produto`) VALUES
+(99278471, 'teste', '20.00', 'teste'),
+(99278472, 'teste2', '10.50', 'teste2');
 
 --
 -- Indexes for dumped tables
@@ -62,6 +83,12 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`codigo_cliente`);
 
 --
+-- Indexes for table `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id_produto`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -69,7 +96,13 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `codigo_cliente` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `codigo_cliente` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99278473;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
