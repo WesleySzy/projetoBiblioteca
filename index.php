6574202fa447b98ba4355f4a4a1d9fea430242f9
nezaodooco <bruce_gabriel_rossini@hotmail.com>
2019-06-26 00:43:41 -0300

    26/06/2019
    Alterações na tela de Index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Blank</title>
+  <title>Szystem</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -148,35 +148,104 @@
                 </form>
               </div>
             </li>
-
-
           </ul>
-
         </nav>
         <!-- End of Topbar -->
 
         <!-- Begin Page Content -->
         <div class="container-fluid">
-
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Relojio</h1>
+        	<h1 class="h1 mb-4 text-gray-800">Relogio</h1> 
+				<center><script type="text/javascript" language="JavaScript">
+					// Função que monta Mês, Dia, e Ano para exibir no HTML
+					// Declaração de Variaveis que receberam os valores:
 
-        <div id="demo">
-          <script type="text/javascript">
-                var myVar = setInterval(myTimer ,1000);
-                function myTimer() {
-                var d = new Date(), displayDate;
-               if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-                displayDate = d.toLocaleTimeString('pt-BR');
-            } else {
-                displayDate = d.toLocaleTimeString('pt-BR', {timeZone: 'America/Belem'});
-             }
-                document.getElementById("demo").innerHTML = displayDate;
-             }
+					var now = new Date();
+					var hours = now.getHours();
+					var minutes = now.getMinutes();
+					var seconds = now.getSeconds();
+					var timeValue = "" + ((hours >12) ? hours -12 :hours)
 
-          </script>
-        </div>         
+					//Tratamento quantidade de Digitos nos Minutos e exibi se é 
+					// dia ou noite, PM, AM.
 
+					timeValue += ((minutes < 10) ? ":0" : ":") + minutes
+					timeValue += ((seconds < 10) ? ":0" : ":") + seconds
+					timeValue += (hours >= 12) ? " PM" : " AM"
+					timerRunning = true;
+					mydate = new Date();
+					myday = mydate.getDay();
+					mymonth = mydate.getMonth();
+					myweekday = mydate.getDate();
+					weekday = myweekday;
+					myyear = mydate.getFullYear();
+					year = myyear;
+
+					// Tratamento dos Dias da Semana
+
+					if(myday == 0)
+					  day = " Domingo, "
+
+					else if(myday == 1)
+					  day = " Segunda Feira, "
+
+					else if(myday == 2)
+					  day = " Terça Feira, "
+
+					else if(myday == 3)
+					  day = " Quarta Feira, "
+
+					else if(myday == 4)
+					  day = " Quinta Feira, "
+
+					else if(myday == 5)
+					  day = " Sexta Feira, "
+
+					else if(myday == 6)
+					  day = " Sábado, "
+
+					//Tratamento dos Meses
+
+					if(mymonth == 0)
+					  month = " de Janeiro  "
+
+					else if(mymonth ==1)
+					  month = " de Fevereiro  "
+
+					else if(mymonth ==2)
+					  month = " de Março  "
+
+					else if(mymonth ==3)
+					  month = " de April "
+
+					else if(mymonth ==4)
+					  month = " de Maio "
+
+					else if(mymonth ==5)
+					  month = " de Junho  "
+
+					else if(mymonth ==6)
+					  month = " de Julho "
+
+					else if(mymonth ==7)
+					  month = " de Agosto  "
+
+					else if(mymonth ==8)
+					  month = " de Setembro  "
+
+					else if(mymonth ==9)
+					  month = " de Outubro  "
+
+					else if(mymonth ==10)
+					  month = " de Novembro "
+
+					else if(mymonth ==11)
+					  month = " de Dezembro "
+
+					//Exibe na Pagina o Resultado concatenando(+) os valores
+
+					document.write( day + myweekday + month + " de " + year + " - " + timeValue);
+				</script></center>        
         </div>
         <!-- /.container-fluid -->
 
