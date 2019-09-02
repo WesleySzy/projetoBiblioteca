94f5d6324c5b4554ed0a4d9da4a336ef9f6bc397
justdarin <darincurso@gmail.com>
2019-09-01 22:13:20 -0300

    BRUCE

--- formulario_cad_livro.php
+++ formulario_cad_livro.php
@@ -7,9 +7,27 @@ $editora=$_POST['editora'];
 $genero=$_POST['genero'];
 $estoque=$_POST['estoque_livro'];
 
+$procurar = "SELECT * FROM livros WHERE isbn = '$isbn' ";
+$verificar = mysqli_query($conexao, $procurar);
+
+if(mysqli_num_rows($verificar) > 0){
+
+echo"<script>window.location='interface_cad_livro.php';
+alert('Livro ja cadastrado!');
+</script>"; 
+
+exit;
+
+} else {
+
 $sql=mysqli_query($conexao, "INSERT INTO livros (isbn, titulo_livro, id_autor, id_editora, id_genero, qtd_disponivel) VALUES ('$isbn', '$titulo', '$autor', '$editora', '$genero', '$estoque')");
+
+mysqli_query($conexao, $sql);
 
 echo"<script>window.location='interface_cad_livro.php';
 alert('Gravado com Sucesso!');
 </script>"; 
+
+mysqli_close($conexao); 
+}
 ?>
