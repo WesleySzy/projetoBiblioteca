1a18b88f64b7b12dd038c280b9ab40765884e4e3
Pedro <42793973+akadarin@users.noreply.github.com>
2019-09-27 20:04:56 -0300

    Atualizações
    Bloqueio de acesso diretamente pela URL

--- view/aluno_view.php
+++ view/aluno_view.php
@@ -1,6 +1,6 @@
 <?php include "../control/conexao_banco.php"; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
 
@@ -15,7 +15,7 @@
     <!-- Fontes para o template -->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
-    href="../https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
 
     <!-- Estilos customizados para o template -->
@@ -35,7 +35,7 @@
         <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: #3578E5">
 
             <!-- Barra Lateral marca -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="aluno_view.php">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="home.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-book-open"></i>
                 </div>
@@ -47,7 +47,7 @@
 
             <!-- Nav bar pagina inicial -->
             <li class="nav-item active">
-                <a class="nav-link" href="aluno_view.php">
+                <a class="nav-link" href="home.php">
                     <i class="fas fa-home"></i>
                     <span>Página Inicial</span></a>
                 </li>
@@ -61,102 +61,85 @@
                 </div>
 
                 <!-- Itens Navbar - juncao de paginas do menu -->
+            <li class="nav-item">
+                <a class="nav-link" href="../sair.php">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span>Sair</span></a>
+                </li>
+
+
+                <!-- Divisor -->
+                <hr class="sidebar-divider d-none d-md-block">
+
+                <!-- Barra lateral ativador -->
+                <div class="text-center d-none d-md-inline">
+                    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                </div>
+
+            </ul>
+            <!-- fim barra lateral -->
+
+
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
+
+                <!-- Main Content -->
+                <div id="content">
+
+                    <!-- Topbar -->
+                    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                        <!-- Sidebar Toggle (Topbar) -->
+                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                            <i class="fa fa-bars"></i>
+                        </button>
 
 
 
-                <!-- Nav bar item - Tabela -->
-                <li class="nav-item">
-                    <a class="nav-link" href="viewLivros.php">
-                        <i class="fas fa-fw fa-table"></i>
-                        <span>Livros</span></a>
+                        <!-- Topbar Navbar -->
+                        <ul class="navbar-nav ml-auto">
+
+                            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                            <li class="nav-item dropdown no-arrow d-sm-none">
+                                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-search fa-fw"></i>
+                            </a>
+                            <!-- Dropdown - Messages -->
+                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                            aria-labelledby="searchDropdown">
+                            <form class="form-inline mr-auto w-100 navbar-search">
+                                <div class="input-group">
+                                    <input type="text" class="form-control bg-light border-0 small"
+                                    placeholder="Pesquise..." aria-label="Search"
+                                    aria-describedby="basic-addon2">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary" type="button">
+                                            <i class="fas fa-search fa-sm"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </li>
 
 
+                </ul>
+                <script type="text/javascript">
+                    var myVar = setInterval(myTimer, 1000);
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="viewEmprestimos.php">
-                            <i class="fas fa-fw fa-table"></i>
-                            <span>Emprestimos</span></a>
-                        </li>
-
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="../sair.php">
-                                <i class="fas fa-sign-out-alt"></i>
-                                <span>Sair</span></a>
-                            </li>
-
-
-                            <!-- Divisor -->
-                            <hr class="sidebar-divider d-none d-md-block">
-
-                            <!-- Barra lateral ativador -->
-                            <div class="text-center d-none d-md-inline">
-                                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-                            </div>
-
-                        </ul>
-                        <!-- fim barra lateral -->
-
-
-                        <!-- Recorte de conteudo -->
-                        <div id="content-wrapper" class="d-flex flex-column">
-
-                            <!-- Conteudo Principal -->
-                            <div id="content">
-
-                                <!-- Barra do topo -->
-                                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                                    <!-- Barra lateral ativador (topo) -->
-                                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                                        <i class="fa fa-bars"></i>
-                                    </button>
-
-                                    <!-- barra de navegacao do topo -->
-                                    <ul class="navbar-nav ml-auto">
-
-                                        <!-- barra de navegacao item - search dropdown -->
-                                        <li class="nav-item dropdown no-arrow d-sm-none">
-                                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-search fa-fw"></i>
-                                        </a>
-                                        <!-- Dropdown mensagem -->
-                                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                        aria-labelledby="searchDropdown">
-                                        <form class="form-inline mr-auto w-100 navbar-search">
-                                            <div class="input-group">
-                                                <input type="text" class="form-control bg-light border-0 small"
-                                                placeholder="Pesquise..." aria-label="Search"
-                                                aria-describedby="basic-addon2">
-                                                <div class="input-group-append">
-                                                    <button class="btn btn-primary" type="button">
-                                                        <i class="fas fa-search fa-sm"></i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </li>
-
-
-                            </ul>
-                            <script type="text/javascript">
-                                var myVar = setInterval(myTimer, 1000);
-
-                                function myTimer() {
-                                    var d = new Date(),
-                                    displayDate;
-                                    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-                                        displayDate = d.toLocaleTimeString('pt-BR');
-                                    } else {
-                                        displayDate = d.toLocaleTimeString('pt-BR', {
-                                            timeZone: 'America/Belem'
-                                        });
-                                    }
-                                    document.getElementById("demo").innerHTML = displayDate;
-                                }
+                    function myTimer() {
+                        var d = new Date(),
+                        displayDate;
+                        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+                            displayDate = d.toLocaleTimeString('pt-BR');
+                        } else {
+                            displayDate = d.toLocaleTimeString('pt-BR', {
+                                timeZone: 'America/Belem'
+                            });
+                        }
+                        document.getElementById("demo").innerHTML = displayDate;
+                    }
 
                     // Função que monta Mês, Dia, e Ano para exibir no HTML
                     // Declaração de Variaveis que receberam os valores:
@@ -241,67 +224,81 @@
                 </script>
                 <div id="demo"></div>
             </nav>
-            <!-- Fim da barra do topo -->
+            <!-- End of Topbar -->
 
-            <!-- Comeco da pagina de conteudo -->
-           <div class="container-fluid">
-               <!-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="img/..." alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="img/..." alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="img/..." alt="Third slide">
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+
+                <!-- DataTables -->
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold" style="color: #3578E5;">Livros</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>ISBN</th>
+                                        <th>Titulo</th>
+                                        <th>Autor</th>
+                                        <th>Editora</th>
+                                        <th>Genero</th>
+                                        <th>Estoque</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php 
+                                    $sql=mysqli_query($conexao, "SELECT * FROM livros JOIN genero ON livros.id_genero = genero.id_genero JOIN editora ON livros.id_editora = editora.id_editora JOIN autor ON livros.id_autor = autor.id_autor WHERE livros.id_livro <> 0");
+                                    while($row = mysqli_fetch_array($sql)){ ?>
+
+                                        <tr>
+                                            <td><?php echo $row['isbn'];?></td>
+                                            <td><?php echo $row['titulo_livro'];?></td>
+                                            <td><?php echo $row['nome_autor'];?></td>
+                                            <td><?php echo $row['nome_editora'];?></td>
+                                            <td><?php echo $row['desc_genero'];?></td>
+                                            <td><?php echo $row['qtd_disponivel'];?></td>
+                                        </tr>
+
+                                    <?php } ?>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
-                    data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
-                data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div> -->
-        <!-- titulo da pagina -->
-
-    </div>
-    <!-- /.container-fluid -->
-
-</div>
-<!-- Fim do conteudo -->
-
-<!-- Rodape -->
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Todos os direitos reservados &copy; Unopar Arapongas 2019</span>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
         </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Todos os direitos reservados &copy; Unopar Arapongas 2019</span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
+
     </div>
-</footer>
-<!-- Fim do rodape -->
+    <!-- End of Content Wrapper -->
 
 </div>
-<!-- Fim do recorte do conteudo -->
+<!-- End of Page Wrapper -->
 
-</div>
-
-
-<!-- Botao de voltar ao topo -->
+<!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
 
+<!--script modal-->
+<script>
+    $('#meuModal').on('shown.bs.modal', function() {
+        $('#meuInput').trigger('focus')
+    })
+</script>
 <!-- Bootstrap core JavaScript-->
 <script src="../vendor/jquery/jquery.min.js"></script>
 <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -311,6 +308,16 @@
 
 <!-- Custom scripts for all pages-->
 <script src="../js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="../js/demo/datatables-demo.js"></script>
+
+<!-- Table translation to portuguese -->
+<script src="../js/portuguese.js"></script>
 
 </body>
 
