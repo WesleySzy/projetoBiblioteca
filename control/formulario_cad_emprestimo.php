c8fcd2fba1fd9361c341c29c05d188882b8f1e53
Wesley Szytka Gomes <wesley_szy@live.com>
2019-09-29 02:20:58 -0300

    verificação empréstimo

--- control/formulario_cad_emprestimo.php
+++ control/formulario_cad_emprestimo.php
@@ -5,9 +5,15 @@ $aluno=$_POST['aluno'];
 $dt_retirada=$_POST['dt_retirada'];
 $dt_entrega=$_POST['dt_entrega'];
 
+if($dt_retirada < $dt_entrega){
 $sql=mysqli_query($conexao, "INSERT INTO emprestimo (id_livro, id_aluno, dt_retirada, dt_entrega) VALUES ('$livro', '$aluno', '$dt_retirada', '$dt_entrega')");
 
 echo"<script>window.location='../view/interface_cad_emprestimo.php';
 alert('Gravado com Sucesso!');
-</script>"; 
+</script>"; }
+else{
+     echo"<script>window.location='../view/interface_cad_emprestimo.php';
+    alert('Data de Devolução é menor que a Data de Retirada!');
+    </script>";
+} 
 ?>
